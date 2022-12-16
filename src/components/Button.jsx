328ff433ed98fb08728c29b1a28a5bb0f5411d5f
ronitafter <54575125+ronitafter@users.noreply.github.com>
2022-12-16 13:10:31 +0200

    ggg

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
-const Button = () => {
+const Button = ({ bgColor, color, size, text, borderRadius }) => {
   return (
-    <div>
-      <h1>Button</h1>
-    </div>
+    <button
+      type="button"
+      style={{ backgroundColor: bgColor, color, borderRadius }}
+      className={` text-${size} p-3 hover:drop-shadow-xl hover:bg-$`}
+    >
+      {text}
+    </button>
   );
 };
 
