9d3ce89ea06e224b1786c7407ad5c3e67fbd526e
ronitafter <54575125+ronitafter@users.noreply.github.com>
2022-12-20 22:50:28 +0200

    ggg

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { ColorPickerComponent } from "@syncfusion/ej2-react-inputs";
+import { Header } from "../components";
 
 const ColorPicker = () => {
   return (
-    <div>
-      <h1>ColorPicker</h1>
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="App" title="Color Picker" />
+      <div className="text-center">
+        <div id="preview" />
+        <div className="flex justify-center items-center gap-20 flex-wrap">
+          <div className="text-2xl font-semibold mt-2 mb-4">
+            <p>Inline Pallet</p>
+            <ColorPickerComponent
+              id="inline-pallete"
+              mode="Palette"
+              modeSwitcher={false}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
