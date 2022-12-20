9c814ce69b424eed8c9830f3944d3efc4a5af6ce
RONIT AFTER <ronitafter@gmail.com>
2022-12-20 12:08:04 +0200

    gfgf

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,9 +1,29 @@
 import React from "react";
-
+import {
+  ScheduleComponent,
+  ViewDirective,
+  ViewsDirTypecast,
+  Day,
+  Week,
+  WorkWeek,
+  Month,
+  Agenda,
+  Inject,
+  Resize,
+  DragAndDrop,
+} from "@syncfusion/ej2-react-schedule";
+import { DatePickerComponent } from "@syncfusion/ej2-react-calendars";
+import { scheduleData } from "../data/dummy";
+import { Header } from "../components";
 const Calendar = () => {
   return (
-    <div>
-      <h1>Calendar</h1>
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="App" title="Calendar" />
+      <ScheduleComponent>
+        <Inject
+          services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]}
+        />
+      </ScheduleComponent>
     </div>
   );
 };
